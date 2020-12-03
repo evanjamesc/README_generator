35ee0c6a40357a22a5f0e37353a184c74a4dfd80
Evan Christianson <evanjameschristianson@gmail.com>
2020-12-03 16:26:21 -0600

    Created first prompts

--- index.js
+++ index.js
@@ -11,11 +11,46 @@ let promptUser = () => {
             message: 'What is the title of your project?',
             name: 'title',
         },
+        // Descripton
+        {
+            type: 'input',
+            message: 'Enter project description:',
+            name: 'description',
+        },
+        // Installation instructions
+        {
+            type: 'input',
+            message: 'Enter installation instructions:',
+            name: 'installation',
+        },
+        // Usage information
+        {
+            type: 'input',
+            message: 'Enter usage instructions:',
+            name: 'usage',
+        },
+        // Contribution guidelines
+        {
+            type: 'input',
+            message: 'Who can contribute to this project?',
+            name: 'contribution',
+        },
+        // Test instructions
+        {
+            type: 'input',
+            message: 'Enter testing instructions for your project:',
+            name: 'testing',
+        },
     ])
-    .then((response) => {
-        console.log(response.title);
-    }
-    );
+        .then((response) => {
+            console.log(response.title);
+            console.log(response.description);
+            console.log(response.installation);
+            console.log(response.usage);
+            console.log(response.contribution);
+            console.log(response.testing);
+        }
+        );
 };
 
 promptUser();
